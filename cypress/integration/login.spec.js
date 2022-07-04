ab566d51dd29c34a619b489d5f4916251462ba1d
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-07-03 22:31:30 -0300

    Validando textos

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -6,6 +6,14 @@ it('deve logar com sucesso', () =>{
  cy.get('#passId').type('pwd123')
  cy.get('button[type=submit]').click();
 
+ cy.get('h3[class^="title"]')
+ .should('have.text','Olá Papito, bem-vindo ao Orkut')
+
+  const expectedText = 'Em breve você poderá participar de comunidades, adicionar amigos e deixar um Scraps. hahahahah'
+
+ cy.get('p[class=subheader]')
+ .should('have.text',expectedText)
+
 
 
 
